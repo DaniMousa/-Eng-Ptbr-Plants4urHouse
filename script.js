5ead67fdb73e9381d757feeb193329070f9caf8a
DaniMousa <danilo.moura.rosa87@gmail.com>
2024-09-28 05:50:24 -0300

    Update script.js
    Fixed grammatical errors

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const enableDarkMode = () => {
     rosesText.innerHTML = `Roses come in many different colors and have their own unique meanings.<br>Pink roses symbolize admiration... Orange roses symbolize passion... Yellow roses symbolize friendship... and the list goes on.`;
     gladiolusText.innerHTML = `These are a type of flower called the gladiolus, also known as the “sword flower”.<br>As you might guess, they represent strength of character. Someone who is honest, with strong moral values... Someone who stands by their convictions...`;
     cactusText.innerHTML = `Cacti are very sturdy and resilient plants by nature. They don't need much care and can survive even in seemingly impossible conditions.`;
-    tulipsText.innerHTML = `And thesse... These are white tulips. ulips aren't very flashy plants. They're not too big or small, not too elaborate or flamboyant.<br>If I could put it in another way... I guess I would say that... they're comfortable. Simple, modest, and perfect.`;
+    tulipsText.innerHTML = `And thesse... These are white tulips. Tulips aren't very flashy plants. They're not too big or small, not too elaborate or flamboyant.<br>If I could put it in another way... I guess I would say that... they're comfortable. Simple, modest, and perfect.`;
     localStorage.setItem('darkMode', 'active');
 }
 
